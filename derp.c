ce74e95262cff5eba27fc9537b3535fdee475e3f
inderpxar <inderpxar@gmail.com>
2024-11-06 21:40:20 -0500

    Scroll win down

--- derp.c
+++ derp.c
@@ -22,6 +22,16 @@ extern const hUGESong_t the_traveller;
 
 uint8_t bSwitchedSound = 0x0;
 uint8_t i, j;
+uint8_t titleTextPosY = 0;
+
+void update_win() {
+  if(titleTextPosY < 144) {
+    titleTextPosY++;
+    move_win(7, titleTextPosY);
+  } else {
+    move_win(7, MAXWNDPOSY + 1);
+  }
+}
 
 void main(void)
 {
@@ -58,12 +68,27 @@ void main(void)
   set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN0);  // Top 12 rows
   set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN0);  // Final four tiles in row 12
   set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN0[4]);  // Bottom 6 rows
+  // Set window tiles
+  set_win_tiles(0, 0, title_textWidth, title_textHeight, title_textPLN0);
   // Set bg attributes
   VBK_REG = VBK_ATTRIBUTES;
-  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN1);
-  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN1);
-  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN1[4]);
-  VBK_REG = VBK_TILES;
+  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN1);  // Top 12 rows
+  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN1);  // Final four tiles in row 12
+  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN1[4]);  // Bottom 6 rows
+  // Set window attributes
+  // VBK_REG = VBK_BANK_1;
+  // for (i = 0; i < titleHeight; i+=2) {
+  //   for (j = 0; j < titleWidth; j+=2) {
+  //     // Second palette for full black
+  //     // VBK_REG = VBK_TILES;
+  //     // set_win_tiles(j, i, 1, 1, 0x00);
+  //     VBK_REG = VBK_ATTRIBUTES;
+  //     set_win_tiles(j, i, 1, 1, 0x09);
+  //   }
+  //   // set_win_tiles(i, 0, 1, title_textHeight, &title_textPLN1[i * title_textHeight]);
+  // }
+  set_win_tiles(0, 0, title_textWidth, title_textHeight, title_textPLN1);
+
 
 
   SCX_REG = 0;
@@ -81,6 +106,7 @@ void main(void)
 
   while(1) {
     vsync();
+    update_win();
     // hUGE_dosound();
     i = joypad();
 
