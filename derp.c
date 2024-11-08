f41ab72b8946e27b512bd035139e3ff68ba0eaa1
inderpxar <inderpxar@gmail.com>
2024-11-08 15:56:00 -0500

    Move stuff around

--- derp.c
+++ derp.c
@@ -37,7 +37,56 @@ uint8_t windowCounter = 0;
 #define NUM_TITLE_TEXT_TILES 54
 uint8_t blinkStartText = 25;
 
-void update_title_win(void) {
+void hUGE_stop_music(void) {
+  // Not sure why this isn't part of the library; mute all channels
+  hUGE_mute_channel(HT_CH1, HT_CH_MUTE);
+  hUGE_mute_channel(HT_CH2, HT_CH_MUTE);
+  hUGE_mute_channel(HT_CH3, HT_CH_MUTE);
+  hUGE_mute_channel(HT_CH4, HT_CH_MUTE);
+}
+
+void init_title(void) {
+  set_bkg_palette(0, 2, title_bg_tiles_lowCGBPal);  // 2 bg palettes
+  set_bkg_palette(2, 1, press_start_textCGBPal);
+
+  // Initialize the background tiles
+  VBK_REG = VBK_BANK_0;
+  gb_decompress_bkg_data(0, title_bg_tiles_low);
+
+  VBK_REG = VBK_BANK_1;
+  gb_decompress_bkg_data(0, title_bg_tiles_hi);
+  gb_decompress_bkg_data(353 - 256, font_tiles);  // Font tiles
+  VBK_REG = VBK_BANK_0;
+
+  // Draw the background
+  VBK_REG = VBK_TILES;
+  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN0);  // Top 12 rows
+  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN0);  // Final four tiles in row 12
+  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN0[4]);  // Bottom 6 rows
+  // Set bg attributes
+  VBK_REG = VBK_ATTRIBUTES;
+  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN1);  // Top 12 rows
+  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN1);  // Final four tiles in row 12
+  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN1[4]);  // Bottom 6 rows
+  // Set window attributes
+  // Second palette for full black
+  tmp = 0x1;
+  for (i = 0; i < titleHeight; i++) {
+    for (j = 0; j < titleWidth; j++) {
+      VBK_REG = VBK_ATTRIBUTES;
+      set_win_tiles(j, i, 1, 1, &tmp);
+    }
+  }
+  VBK_REG = VBK_TILES;
+
+  SCX_REG = 0;
+  SCY_REG = 0;
+
+  // Start title screen music
+  hUGE_init(&space_odyssey);
+}
+
+void update_title_win(uint8_t input) {
   windowCounter++;
   if (windowCounter > TITLE_VSYNC_FRAMES) {
     windowCounter = 0;
@@ -75,15 +124,22 @@ void update_title_win(void) {
       }
     }
   }
+
+  if (input & J_START) {
+    gamestate = STATE_PLAY;
+    // Stop title screen music
+    hUGE_stop_music();
+    // Start game music
+    hUGE_init(&the_traveller);
+  }
 }
 
 void main(void)
 {
   disable_interrupts();
   DISPLAY_OFF;
-  set_bkg_palette(0, 2, title_bg_tiles_lowCGBPal);  // 2 bg palettes
-  set_bkg_palette(2, 1, press_start_textCGBPal);
   LCDC_REG = LCDCF_OFF | LCDCF_WIN9C00 | LCDCF_WINON | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_OBJ16 | LCDCF_OBJON | LCDCF_BGON;
+
   /*
    * LCD        = Off
    * WindowBank = 0x9C00
@@ -94,70 +150,23 @@ void main(void)
    * OBJ        = On
    * BG         = On
    */
+
+  // Init sound playback
   NR52_REG = 0x80;
   NR51_REG = 0xFF;
   NR50_REG = 0x77;
 
-  // Initialize the background tiles
-  VBK_REG = VBK_BANK_0;
-  gb_decompress_bkg_data(0, title_bg_tiles_low);
-
-  VBK_REG = VBK_BANK_1;
-  gb_decompress_bkg_data(0, title_bg_tiles_hi);
-  gb_decompress_bkg_data(353 - 256, font_tiles);  // Font tiles
-  VBK_REG = VBK_BANK_0;
-
-  // Draw the background
-  VBK_REG = VBK_TILES;
-  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN0);  // Top 12 rows
-  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN0);  // Final four tiles in row 12
-  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN0[4]);  // Bottom 6 rows
-  // Set bg attributes
-  VBK_REG = VBK_ATTRIBUTES;
-  set_bkg_tiles(0, 0, titleWidth, titleHeight - 5, titleBLK0PLN1);  // Top 12 rows
-  set_bkg_tiles(titleWidth - 4, titleHeight - 6, 4, 1, titleBLK1PLN1);  // Final four tiles in row 12
-  set_bkg_tiles(0, titleHeight - 5, titleWidth, 5, &titleBLK1PLN1[4]);  // Bottom 6 rows
-  // Set window attributes
-  // Second palette for full black
-  tmp = 0x1;
-  for (i = 0; i < titleHeight; i++) {
-    for (j = 0; j < titleWidth; j++) {
-      VBK_REG = VBK_ATTRIBUTES;
-      set_win_tiles(j, i, 1, 1, &tmp);
-    }
-  }
-  VBK_REG = VBK_TILES;
-
-  SCX_REG = 0;
-  SCY_REG = 0;
+  init_title();
 
   DISPLAY_ON;
   enable_interrupts();
 
-  // Start title screen music
-  hUGE_init(&space_odyssey);
-
   while(1) {
     vsync();
+    i = joypad();
     if (gamestate == STATE_START) {
-      update_title_win();
+      update_title_win(i);
     }
     hUGE_dosound();
-    i = joypad();
-
-    if(i & J_SELECT) {
-      if(bSwitchedSound == 0x0) {
-        if(i & J_B) {
-          hUGE_init(&pressure_cooker);
-        } else if (i & J_A) {
-          hUGE_init(&the_traveller);
-        } else {
-          hUGE_init(&space_odyssey);
-        }
-        bSwitchedSound = 0x1;
-      }
-    } else {
-      bSwitchedSound = 0x0;
-    }
   }
 }
