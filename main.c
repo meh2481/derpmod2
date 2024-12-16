4c5409d756dda8ff63d90502fd4415206306a1b0
inderpxar <inderpxar@gmail.com>
2024-12-16 17:22:08 -0500

    huh it works better with faster CPU, who'd have thought

--- main.c
+++ main.c
@@ -23,6 +23,8 @@ uint8_t gamestate;
 void main(void)
 {
   disable_interrupts();
+  set_default_palette();
+  cpu_fast();
   DISPLAY_OFF;
   LCDC_REG = LCDCF_OFF | LCDCF_WIN9C00 | LCDCF_WINON | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_OBJ16 | LCDCF_OBJON | LCDCF_BGON;
 
